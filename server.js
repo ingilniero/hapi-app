5e734643e2564b748f757885ebceebb49fff95f7
Gilberto Villa <ingilniero@gmail.com>
2015-09-17 11:30:56 -0500

    Add vision npm package

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ var server = new Hapi.Server();
 
 var cards = {};
 
+server.register([require('inert'), require('vision')], function(err) {
+  if (err) {
+    console.log('Failed to load plugin:', err);
+  }
+});
+
 server.connection({ port: 4000 });
 
 server.ext('onRequest', function(request, reply) {
   console.log('Request received: ' + request.path);
   reply.continue();
-});
-
-server.register(require('inert'), function(err) {
-  if (err) {
-    console.log('Failed to load plugin:', err);
-  }
 });
 
 server.route({
