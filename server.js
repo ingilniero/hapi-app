441e9fbcef3ce3f262875bc7b482f5359c276d68
Gilberto Villa <ingilniero@gmail.com>
2015-09-20 23:35:02 -0500

    Refactor application structure

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 var Hapi = require('hapi'),
-    uuid = require('uuid'),
-    Joi  = require('joi'),
-    Boom = require('boom'),
-    Good = require('good'),
-    fs   = require('fs');
+    CardStore = require('./lib/cardStore');
 
 var server = new Hapi.Server();
 
-var cards = loadCards();
+CardStore.initialize();
 
 server.register([
   { register: require('inert'), options: {} },
@@ -69,103 +65,7 @@ server.ext('onPreResponse', function(request, reply) {
   reply.continue();
 });
 
-server.route({
-  path: '/',
-  method: 'GET',
-  handler: {
-    file: 'templates/index.html'
-  }
-});
-
-server.route({
-  path: '/assets/{path*}',
-  method: 'GET',
-  handler: {
-    directory: {
-      path: './public',
-      listing: false
-    }
-  }
-});
-
-server.route({
-  path: '/cards/new',
-  method: ['GET', 'POST'],
-  handler: newCardHandler
-})
-
-server.route({
-  path: '/cards',
-  method: 'GET',
-  handler: cardsHandler
-});
-
-server.route({
-  path: '/cards/{id}',
-  method: 'DELETE',
-  handler: deleteCardHandler
-})
-
-var cardSchema = Joi.object().keys({
-  name: Joi.string().min(3).max(50).required(),
-  recipient_email: Joi.string().email().required(),
-  sender_name: Joi.string().min(3).max(50).required(),
-  sender_email: Joi.string().email().required(),
-  card_image: Joi.string().regex(/.+\.(jpg|bmp|png|gif)\b/).required()
-});
-
-function newCardHandler(request, reply) {
-  if(request.method === 'get') {
-    reply.view('new', { card_images: mapImages() });
-  } else {
-    Joi.validate(request.payload, cardSchema, function(err, val) {
-      if(err) {
-        console.log(err);
-        return reply(Boom.badRequest(err.details[0].message));
-      }
-
-      var card = {
-        name: val.name,
-        recipient_email: val.recipient_email,
-        sender_name: val.sender_name,
-        sender_email: val.sender_email,
-        card_image: val.card_image
-      };
-
-      saveCard(card);
-
-      console.log(cards);
-
-      reply.redirect('/cards');
-    });
-  }
-}
-
-function cardsHandler(request, reply) {
-  reply.view('cards', { cards: cards });
-}
-
-function deleteCardHandler(request, reply) {
-  delete cards[request.params.id];
-  reply();
-}
-
-function saveCard(card) {
-  var id = uuid.v1();
-
-  card.id = id;
-  cards[id] = card;
-}
-
-function loadCards() {
-  var file = fs.readFileSync('./cards.json');
-
-  return JSON.parse(file.toString());
-}
-
-function mapImages() {
-  return fs.readdirSync('./public/images/cards');
-}
+server.route(require('./lib/routes'));
 
 server.start(function (err) {
   if (err) {
